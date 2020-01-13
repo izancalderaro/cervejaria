2fa9e9a42ce7f2e3f52dc009cf31c28691ddca7d
IC <izancalderaro@yahoo.com.br>
2020-01-13 17:02:01 -0400

    cervejaria 3.4

--- src/main/java/com/sfe/cervejaria/controller/CervejasController.java
+++ src/main/java/com/sfe/cervejaria/controller/CervejasController.java
@@ -1,8 +1,13 @@
 package com.sfe.cervejaria.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CervejasController {
-
+    
+	@GetMapping("/cervejas/novo")
+	public String novo() {
+		return "cerveja/CadastroCerveja";
+	}
 }
