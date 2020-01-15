02093afe19b8d5cd385c849c82850abce5cc2639
IC <izancalderaro@yahoo.com.br>
2020-01-15 13:38:05 -0400

    cervejaria 3.7

--- src/main/java/com/sfe/cervejaria/controller/CervejasController.java
+++ src/main/java/com/sfe/cervejaria/controller/CervejasController.java
@@ -1,13 +1,35 @@
 package com.sfe.cervejaria.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.sfe.cervejaria.model.Cerveja;
 
 @Controller
 public class CervejasController {
-    
-	@RequestMapping("/cervejas/novo")
+
+	/* @RequestMapping("/cervejas/novo") */
+	@GetMapping("/cervejas/novo")
 	public String novo() {
+		System.out.println("get");
 		return "cerveja/CadastroCerveja";
 	}
+
+//	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	@PostMapping("/cervejas/novo") 
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result) {
+
+		if (result.hasErrors()) {
+			System.out.println("Erro na validação do formulário");
+		} 
+
+		System.out.println(">>> sku: " + cerveja.getSku());
+		return "cerveja/CadastroCerveja";		
+
+	}
+
 }
---
+++ src/main/java/com/sfe/cervejaria/model/Cerveja.java
@@ -0,0 +1,50 @@
+package com.sfe.cervejaria.model;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+public class Cerveja {
+
+	@NotBlank
+	public String sku;
+	public String nome;
+	
+	public String getSku() {
+		return sku;
+	}
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((sku == null) ? 0 : sku.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cerveja other = (Cerveja) obj;
+		if (sku == null) {
+			if (other.sku != null)
+				return false;
+		} else if (!sku.equals(other.sku))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
