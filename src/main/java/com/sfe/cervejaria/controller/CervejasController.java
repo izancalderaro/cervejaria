c4142693c9982349f3137bdb2bb3ada740aeb612
IC <izancalderaro@yahoo.com.br>
2020-01-16 12:08:00 -0400

    cervejaria 3.8

--- src/main/java/com/sfe/cervejaria/controller/CervejasController.java
+++ src/main/java/com/sfe/cervejaria/controller/CervejasController.java
@@ -3,9 +3,11 @@ package com.sfe.cervejaria.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.sfe.cervejaria.model.Cerveja;
 
@@ -21,14 +23,17 @@ public class CervejasController {
 
 //	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
 	@PostMapping("/cervejas/novo") 
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result) {
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes redirectAttributes) {
 
 		if (result.hasErrors()) {
-			System.out.println("Erro na validação do formulário");
+			model.addAttribute("mensagem", "SKU não pode ser vazio");			
+			return "cerveja/CadastroCerveja";
 		} 
-
-		System.out.println(">>> sku: " + cerveja.getSku());
-		return "cerveja/CadastroCerveja";		
+		
+		//Salvar no banco de dados
+		
+		redirectAttributes.addFlashAttribute("mensagem", "Cerveja cadastrada com sucesso");
+		return "redirect:/cervejas/novo";		
 
 	}
 
